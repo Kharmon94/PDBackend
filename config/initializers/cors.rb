a5f7deeba424c1c45552328eed1d4e13a38c279a
Kharmon <Kyleharmon52@gmail.com>
2025-11-04 16:10:23 -0500

    back_pro5

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,14 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Allow requests from localhost for development
+    # Allow requests from production domains
     origins 'https://www.preferred.deals', 'https://preferred.deals'
     
     # Allow requests from Railway domains
     origins /.*\.railway\.app$/, /.*\.railway\.dev$/
+    
+    # Allow requests from localhost for development
+    origins 'http://localhost:3000', 'http://localhost:5173'
 
     resource "*",
       headers: :any,
